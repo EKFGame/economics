419bb8011e32888074926f3dcde1dc1e6ced0d5f
Tilindis <heronightmares@gmail.com>
2021-10-05 18:52:02 +0300

    lift doors are made

--- screens/navigation/lift.js
+++ screens/navigation/lift.js
@@ -1,25 +1,62 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, Dimensions, TouchableOpacity, ImageBackground } from "react-native";
+import { StyleSheet, Text, Dimensions, TouchableOpacity, Image, ImageBackground } from "react-native";
 
 const screen = Dimensions.get("window");
-const imageBack = require("../../images/liftas.jpg");
 
 class Lift extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      imageBack: require("../../images/liftfull.png"),
+      doors: require('../../images/liftfull.png'),
+      doorsAreClosed: true,
+      showDors: true,  
     };
   }
 
+  componentDidMount(){
+    this.openLiftDoors();
+  }
+  
+
+  openLiftDoors = () => {
+    if(this.state.doorsAreClosed == true)
+    {
+      setTimeout(() => {
+        this.setState({imageBack: require('../../images/liftas.jpg')})
+        }, 600);  
+        setTimeout(() => {
+            this.setState({doors: require('../../images/lift14.png')})
+        }, 1000);
+        setTimeout(() => {
+            this.setState({doors: require('../../images/lift12.png')})
+        }, 1600);
+        setTimeout(() => {
+            this.setState({doors: require('../../images/lift34.png')})
+        }, 2200);
+        setTimeout(() => {
+            this.setState({showDors: false})
+        }, 2800);
+    }
+    this.setState({doorsAreClosed: false});
+};
+
   render() {
+    
+    const imageShow = () => {
+      if (this.state.showDors == true)
+      return <Image source={this.state.doors} style={styles.image} resizeMode="cover" ></Image>
+      else return
+    }
+
     return (
       <ImageBackground
-      source={imageBack}
+      source={this.state.imageBack}
+      defaultSource={this.state.imageBack}
       style={styles.image}
       resizeMode="stretch"
       >
-          <Text>Something</Text>
+      {imageShow()}
       </ImageBackground>
     );
   }
